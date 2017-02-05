cfbe492750be44b4410c72c1549e71ce95ff731a
liuguangjie <1316421727@qq.com>
2017-02-05 21:27:07 +0800

    2017年02月05日21:26:55 代码演示

--- src/main/java/cn/springmvc/annotation/StudentAction.java
+++ src/main/java/cn/springmvc/annotation/StudentAction.java
@@ -8,8 +8,12 @@ import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Enumeration;
+import java.util.Map;
 
 /**
  * Created by free on 17-2-3.
@@ -19,20 +23,37 @@ public class StudentAction  {
 
 
     @RequestMapping("/editstudent")
-    public String editstudent(Model model){
+    public String editstudent(Model model, HttpServletRequest request, HttpServletResponse response){
         Student student=new Student();
         student.setAge(22);
         student.setName("刘光杰");
         student.setBirthday(new Date());
         model.addAttribute("student",student);
+        System.out.println("查询的uri="+request.getRequestURI());
+        System.out.println(getDescription(request.getAttributeNames(),request));
         return "edit";
+    }
+
+    public String getDescription(Enumeration<?> stringEnumeration,HttpServletRequest request){
+        StringBuilder sb = new StringBuilder();
+        while (stringEnumeration !=null && stringEnumeration.hasMoreElements()){
+            String name=(String)stringEnumeration.nextElement();
+            sb.append(name+"="+request.getAttribute(name)+" \n ");
+        }
+        return sb.toString();
+
     }
 
 
     @RequestMapping("/savestudent")
     public String savestudent(Student student){
+        System.out.println("学生信息 = "+student);
+        System.out.println("map 的值...");
+        /*for (Map.Entry<String,Object> hashEntry:params.entrySet()){
+            System.out.print(hashEntry.getKey()+"="+hashEntry.getValue()+"\t");
+        }*/
 
-        System.out.println(student);
+
         return "hello";
     }
 
